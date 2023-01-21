894bc8c8dc75f483c6064f49d221bc448c88298a
SHAFI <118980998+mmshafi22@users.noreply.github.com>
2023-01-21 20:50:18 +0530

    Update index.js

--- src/components/SimilarProductItem/index.js
+++ src/components/SimilarProductItem/index.js
@@ -1,1 +1,29 @@
-// Write your code here
+import './index.css'
+
+const SimilarProductItem = props => {
+  const {product} = props
+  const {imageUrl, title, price, brand, rating} = product
+  return (
+    <li className="product-list-item">
+      <img
+        src={imageUrl}
+        alt={`similar product ${title}`}
+        className="similar-img"
+      />
+      <h1 className="similar-title">{title}</h1>
+      <p className="similar-brand">by {brand}</p>
+      <div className="rating-price">
+        <p className="similar-price">Rs {price}</p>
+        <div className="rating">
+          <p>{rating}</p>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/star-img.png "
+            alt="star"
+          />
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default SimilarProductItem
